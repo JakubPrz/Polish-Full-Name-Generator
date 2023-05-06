3e6e4b9d8c802f273d3b147b4643c98177d5e27f
JakubPrz <kubaprzepiorka123@gmail.com>
2023-05-06 16:25:04 +0200

    version up

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='pfng',
-    version='1.0.0',
+    version='1.0.1',
     description='Polish full names generator',
     long_description=(Path(__file__).parent / "README.md").read_text(encoding="utf-8"),
     long_description_content_type="text/markdown",
