bc678d4319169d1e14967c4a2dddc96cfe9fd11d
JakubPrz <kubaprzepiorka123@gmail.com>
2023-05-18 21:47:37 +0200

    added `number` parameter validation

--- pfng/generator.py
+++ pfng/generator.py
@@ -12,6 +12,7 @@ def generate_full_names(number: int, gender: str = 'M&F') -> List[str]:
         gender (optional): The gender of full names (M, F, M&F)
 
     Raises:
+        ValueError: If number of full names is < 1
         TypeError: If invalid gender was given.
 
     Returns:
@@ -24,8 +25,11 @@ def generate_full_names(number: int, gender: str = 'M&F') -> List[str]:
         'M&F': None
     }
 
+    if number < 1:
+        raise ValueError('Number of full names must be > 0 !')
+
     if gender not in gender_map.keys():
-        raise TypeError("Provide valid gender type!")
+        raise TypeError('Provide valid gender type !')
 
     full_names = []
     for i in range(number):
